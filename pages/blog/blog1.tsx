376107c03ce6c597b7b057b68f9ea33af13afc0e
Manisha Sorout <soroutmanisha12@gmail.com>
2023-01-24 02:09:28 +0530

    Update blog1.tsx

--- pages/blog/blog1.tsx
+++ pages/blog/blog1.tsx
@@ -1,5 +1,6 @@
 //these are the nested routing
 //like when we type /blog/blog1 in the url we will reach to this page
+// these are the child routing where /blog is the parent path
 
 const blog1 = () => {
   return (
