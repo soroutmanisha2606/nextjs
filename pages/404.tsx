433a7c482d75f689fdff4770fd40b6aa767dc851
manisha <soroutmanisha12@gmail>
2023-01-24 02:04:41 +0530

    adding own 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Erropage = () => {
+  return (<>
+    <div>page not found</div>
+    <div>
+        <a href='/Home'>back to home page</a>
+    </div>
+    </>
+  )
+}
+
+export default Erropage
