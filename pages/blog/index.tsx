96c3ef96b2cc45c136ac252308c8e1cc432dccec
Manisha Sorout <soroutmanisha12@gmail.com>
2023-01-24 02:06:45 +0530

    Update index.tsx

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,5 +1,5 @@
 //file name should be same as the url path is same as the file name
-
+//like here the file name is blog so url path would be '/blog'
 const blog = () => {
   return (
     <div>blog1</div>
